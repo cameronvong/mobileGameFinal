b9936cee415376ab976e96af9b3a63c66cc567f4
mohammedajao <ajaomohammed@gmail.com>
2022-11-17 22:46:33 -0500

    Added remote event system

---
+++ Assets/Code/Util/BunnyMessage.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BunnyMessagePriority
+{
+    LOW,
+    MEDIUM,
+    HIGH
+}
+
+public class BunnyMessage<T>
+{
+    public T payload;
+    public object sender;
+
+    public BunnyMessage()
+    {
+        payload = default(T);
+        sender = BunnyEventManager.Instance;
+    }
+
+    public BunnyMessage(
+        T value,
+        object source
+    ) 
+    {
+        payload  = value;
+        sender = source;
+    }
+}
