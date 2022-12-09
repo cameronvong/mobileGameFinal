427de61dcf9c24b8bc19ecaa7cf7e6007e0250d8
jx1279 <jx1279@nyu.edu>
2022-12-09 11:38:02 -0500

    working on boss death

---
+++ Assets/Code/DefeatedBoss.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Bunny;
+using Bunny.Tools;
+
+public class DefeatedBoss : MonoBehaviour
+{
+    public string nextLevel = "SkullBoss";
+    // Start is called before the first frame update
+    private void Awake() {
+        BunnyEventManager.Instance.RegisterEvent("OnBossHurt", this);
+        callback = sceneTransition;
+    }
+
+    private void Start() {
+        BunnyEventManager.Instance.OnEventRaised<float>("OnBossHurt", callback);
+    }
+
+    public void sceneTransition() {
+        StartCoroutine(deathAnimWait());
+    }
+    
+    IEnumerator deathAnimWait() {
+        yield return new WaitForSecondsRealtime(3.2f);
+        SceneManager.LoadScene("FloatingHeadsScene");
+
+    }
+}
