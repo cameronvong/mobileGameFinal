93027ad027005e6366ff3e96d24dc1d528c17784
jx1279 <jx1279@nyu.edu>
2022-12-09 01:10:40 -0500

    Updatest to freeze frame for testing

--- Assets/Code/FreezeFrame.cs
+++ Assets/Code/FreezeFrame.cs
@@ -7,7 +7,7 @@ using Bunny.Tools;
 
 public class FrameFreeze : MonoBehaviour
 {
-    bool waiting = false;
+    bool waiting;
     public Action<BunnyMessage<float>> callback;
 
     private void Awake() {
@@ -16,20 +16,23 @@ public class FrameFreeze : MonoBehaviour
     }
 
     private void Start() {
+        waiting = false;
         BunnyEventManager.Instance.OnEventRaised<float>("OnBossHurt", callback);
     }
 
     // Start is called before the first frame update
     public void FeedbackFreeze(BunnyMessage<float> message) {
-        if (!waiting) {
-            Time.timeScale = 0.0f;
-            StartCoroutine(WaitTime());
+        if (waiting == false) {
+            return;
         }
+        Time.timeScale = 0.0f;
+        StartCoroutine(WaitTime());
+        
     }
 
     IEnumerator WaitTime() {
         waiting = true;
-        yield return new WaitForSecondsRealtime(0.01f);
+        yield return new WaitForSecondsRealtime(3.0f);
         Time.timeScale = 1.0f;
         waiting = false;
     }
