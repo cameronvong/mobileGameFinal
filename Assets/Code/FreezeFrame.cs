3949e9490d2641c41d957d1ff46dc1b482785a8e
jx1279 <jx1279@nyu.edu>
2022-12-09 00:00:46 -0500

    Freeze Frame Added

---
+++ Assets/Code/FreezeFrame.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Bunny;
+using Bunny.Tools;
+
+public class FrameFreeze : MonoBehaviour
+{
+    bool waiting;
+    public Action<BunnyMessage<float>> callback;
+
+    private void Awake() {
+        BunnyEventManager.Instance.RegisterEvent("OnBossHurt", this);
+        callback = FeedbackFreeze;
+    }
+
+    private void Start() {
+        BunnyEventManager.Instance.OnEventRaised<float>("OnBossHurt", callback);
+    }
+
+    // Start is called before the first frame update
+    public void FeedbackFreeze() {
+        if (!waiting) {
+            Time.timeScale = 0.0f;
+            StartCoroutine(WaitTime(0.01f));
+        }
+    }
+
+    IEnumerator WaitTime() {
+        waiting = true;
+        yield return new WaitForSecondsRealtime(0.01f);
+        time.timeScale = 1.0f;
+        waiting = false;
+    }
+
+}
